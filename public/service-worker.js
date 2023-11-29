50d0c1321d3ed9776d00212326bccbbf4b98b03d
gaurav360core <gaurav.talesara@360coreinc.com>
2023-11-29 18:24:19 +0530

    Update service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,21 +1,20 @@
 // public/service-worker.js
 self.addEventListener('install', event => {
-    event.waitUntil(
-      caches.open('v1').then(cache => {
-        return cache.addAll([
-          '/path/to/your/script.js',  // Add paths to assets you want to cache
-          '/path/to/another/asset',
-          // Add other assets like CSS, images, etc.
-        ]);
-      })
-    );
-  });
-  
-  self.addEventListener('fetch', event => {
-    event.respondWith(
-      caches.match(event.request).then(response => {
-        return response || fetch(event.request);
-      })
-    );
-  });
-  
+  event.waitUntil(
+    caches.open('v1').then(cache => {
+      return cache.addAll([
+        '/otp', // Cache the root page
+        '/.next/static/style.css', 
+        '/images/logo.png', 
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
