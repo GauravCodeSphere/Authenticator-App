c29572aa4d730b35d5d876de88903de8f486e4dc
gaurav360core <gaurav.talesara@360coreinc.com>
2023-12-02 13:15:35 +0530

    aa

--- pages/_app.js
+++ pages/_app.js
@@ -7,7 +7,7 @@ export default function App({ Component, pageProps }) {
       window.addEventListener('load', function() {
         navigator.serviceWorker.register('/service-worker.js')
           .then(function(registration) {
-            console.log('Service Worker registered with scope:', registration.scope);
+            console.log('Service Worker registered with scope:', registration);
           }, function(err) {
             console.log('Service Worker registration failed:', err);
           });
--- public/service-worker.js
+++ public/service-worker.js
@@ -1,20 +1,26 @@
-// public/service-worker.js
-self.addEventListener('install', event => {
+const CACHE_NAME = 'v1';
+const urlsToCache = [
+  '/otp',
+];
+
+self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open('v1').then(cache => {
-      return cache.addAll([
-        '/otp', // Cache the root page
-        '/.next/static/style.css', 
-        '/images/logo.png', 
-      ]);
-    })
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache);
+      })
   );
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request)
+      .then((response) => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
   );
 });
